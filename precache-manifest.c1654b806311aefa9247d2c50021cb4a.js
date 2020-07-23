af87467448d23d6977e21aa84b2127e5e40167fc
Dawid Rynkal <dawidos14051997@gmail.com>
2020-07-23 11:00:48 +0200

    Updates

---
+++ precache-manifest.c1654b806311aefa9247d2c50021cb4a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68464534cab42c9c32ebd973fa45c8a5",
+    "url": "/ShoppingCart/index.html"
+  },
+  {
+    "revision": "3ad3ee1013e9e72aa2ff",
+    "url": "/ShoppingCart/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "7db7908a2202114d5384",
+    "url": "/ShoppingCart/static/css/main.eed8f272.chunk.css"
+  },
+  {
+    "revision": "3ad3ee1013e9e72aa2ff",
+    "url": "/ShoppingCart/static/js/2.15486ae3.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/ShoppingCart/static/js/2.15486ae3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7db7908a2202114d5384",
+    "url": "/ShoppingCart/static/js/main.50a19e31.chunk.js"
+  },
+  {
+    "revision": "4edc72a8f224b0c5ac51",
+    "url": "/ShoppingCart/static/js/runtime-main.49c492c0.js"
+  },
+  {
+    "revision": "23d0ef2fec40e345cf49e27288823579",
+    "url": "/ShoppingCart/static/media/product.23d0ef2f.jpeg"
+  }
+]);
